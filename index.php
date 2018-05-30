1f159055bafc716316bb1432c5ac7bed13b5db3e
b3n10 <francisben.lleve@gmail.com>
2018-05-30 12:08:40 +0800

    Add HTML

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title>Project 1</title>
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<div class='div-body'>
+		<p id='p-text'>
+		I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.
+		</p>
+		<p class="p-author">
+		- Galatians 2:20
+		</p>
+	</div>
+	<div class="div-button">
+		<button id='btn-next'>Next</button>
+	</div>
+	<script src="main.js">
+	</script>
+</body>
+</html>
