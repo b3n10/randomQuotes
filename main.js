2d58e9b1c8b8dade760981d78eaf6334152e022f
b3n10 <francisben.lleve@gmail.com>
2018-05-30 12:08:54 +0800

    Animation left

---
+++ main.js
@@ -0,0 +1,7 @@
+const body = document.getElementsByClassName('div-body')[0];
+const button = document.getElementById('btn-next');
+
+button.addEventListener('click', () => {
+	body.style.left = '-365px';
+});
+
