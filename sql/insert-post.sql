93e8ccd79cdd7abdfa5ec0bf5bae6215bb9e3d90
b3n10 <francisben.lleve@gmail.com>
2018-06-05 02:56:17 +0800

    Fix comma

--- sql/insert-post.sql
+++ sql/insert-post.sql
@@ -6,5 +6,5 @@ INSERT INTO posts (
 	body
 ) VALUES (
 	'Galatians 2:20',
-	'I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.',
+	'I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.'
 );
