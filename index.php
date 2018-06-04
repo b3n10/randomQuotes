dcafc38763bc55216825df3681defac4a1e0aa9d
b3n10 <francisben.lleve@gmail.com>
2018-06-04 20:30:19 +0800

    Add container div

--- index.php
+++ index.php
@@ -7,16 +7,18 @@
 	<link rel="stylesheet" href="style.css">
 </head>
 <body>
-	<div class='div-body'>
-		<p id='p-text'>
-		I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.
-		</p>
-		<p class="p-author">
-		- Galatians 2:20
-		</p>
-	</div>
-	<div class="div-button">
-		<button id='btn-next'>Next</button>
+	<div class="container">
+		<div class='div-body'>
+			<p id='p-text'>
+			I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.
+			</p>
+			<p class="p-author">
+			- Galatians 2:20
+			</p>
+		</div>
+		<div class="div-button">
+			<button id='btn-next'>Next</button>
+		</div>
 	</div>
 	<script src="main.js">
 	</script>
