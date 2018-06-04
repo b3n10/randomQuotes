68852979a96e493d97dc1e69bcaff508a0d0791b
b3n10 <francisben.lleve@gmail.com>
2018-06-05 02:52:29 +0800

    Update left position

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ const body = document.getElementsByClassName('div-body')[0];
 const button = document.getElementById('btn-next');
 
 button.addEventListener('click', () => {
-	body.style.left = '-365px';
+	body.style.left = '-565px';
 });
 
