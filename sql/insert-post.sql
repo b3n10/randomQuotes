f2f9bcadcb01e0af02f4ad1c0f3e10750b74b3c0
b3n10 <francisben.lleve@gmail.com>
2018-06-05 02:54:44 +0800

    Update insert posts

--- sql/insert-post.sql
+++ sql/insert-post.sql
@@ -1,12 +1,10 @@
-USE authentication_system;
+USE quotes;
 
 -- init values name, email only --
-INSERT INTO users (
-	name,
-	email,
-	password
+INSERT INTO posts (
+	title,
+	body
 ) VALUES (
-	'ben',
-	'ben@email.com',
-	'password'
+	'Galatians 2:20',
+	'I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.',
 );
