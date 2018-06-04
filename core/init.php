19e839e9fa74d3a4db8c5de9beda3bfd93efac7b
b3n10 <francisben.lleve@gmail.com>
2018-06-05 02:50:27 +0800

    Add config to Session

--- core/init.php
+++ core/init.php
@@ -1,5 +1,12 @@
 <?php
 
+$_SESSION['config'] = [
+	'host'			=>	'127.0.0.1',
+	'dbname'		=>	'quotes',
+	'user'			=>	'root',
+	'password'	=>	'jairah'
+];
+
 spl_autoload_register(function($class) {
 	require_once "classes/" . $class . ".php";
 });
