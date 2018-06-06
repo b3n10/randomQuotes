5d60a63a063bc6d839566fb47ede289df2e12ae9
b3n10 <francisben.lleve@gmail.com>
2018-06-06 22:02:06 +0800

    Update insert sql

--- sql/insert-post.sql
+++ sql/insert-post.sql
@@ -5,6 +5,6 @@ INSERT INTO posts (
 	title,
 	body
 ) VALUES (
-	'Galatians 2:20',
-	'I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me.'
+	"J.R.R. Tolkien",
+	"For myself, I find I become less cynical rather than more--remembering my own sins and follies; and realize that men's hearts are not often as bad as their acts, and very seldom as bad as their words."
 );
