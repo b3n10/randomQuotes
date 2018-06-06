f9846c2df948ad7aed7687f9783f5f83a9d0be25
b3n10 <francisben.lleve@gmail.com>
2018-06-06 21:50:36 +0800

    Only pass results as array

--- classes/Quote.php
+++ classes/Quote.php
@@ -8,11 +8,10 @@ class Quote {
 		$this->_db = $db;
 	}
 
-	public function get($item) {
+	public function get() {
 
-		// get first index (which is an obj) of returned array
-		// then access the $item from obj
-		return $this->_db->find($item)[0]->{$item};
+		// pass results array
+		return $this->_db->find();
 
 	}
 
