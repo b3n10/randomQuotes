646da0d77277f16cc514b5382311d7f144b4f181
b3n10 <francisben.lleve@gmail.com>
2018-06-07 00:48:05 +0800

    Add delay in animation and positioning

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-const body = document.getElementsByClassName('div-body')[0];
+const txt_body = document.getElementsByClassName('div-maintext')[0];
 const button = document.getElementById('btn-next');
 
 button.addEventListener('click', () => {
-	body.style.left = '-565px';
+
+	txt_body.style.left = '-565px';
+
+	setTimeout(() => {
+
+		if (txt_body.style.left === '-565px') {
+
+			txt_body.style.display = 'none';
+
+			setTimeout(() => {
+
+				txt_body.style.left = '565px';
+				txt_body.style.display = 'block';
+
+				setTimeout(() => {
+
+					txt_body.style.left = '0px';
+
+				}, 500);
+
+			}, 100);
+
+		}
+
+	}, 500);
+
 });
 
