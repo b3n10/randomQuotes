b4c64ec1a2b489be14625a4a876381a46bdca700
b3n10 <francisben.lleve@gmail.com>
2018-06-06 21:09:14 +0800

    Add get method

--- classes/Quote.php
+++ classes/Quote.php
@@ -5,7 +5,18 @@ class Quote {
 	private $_db = null;
 
 	public function __construct($db) {
-		$this->_db = $db
+		$this->_db = $db;
+	}
+
+	public function get($item) {
+
+		// $id = random_number()
+		$id = 1;
+
+		// get first index (which is an obj) of returned array
+		// then access the $item from obj
+		return $this->_db->find($item, $id)[0]->{$item};
+
 	}
 
 }
