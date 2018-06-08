df32a4a34510d431d0f47b1513c69ddaa9ca91a2
b3n10 <francisben.lleve@gmail.com>
2018-06-08 11:15:07 +0800

    Update babel compiled js

--- libs/js/dist/main.js
+++ libs/js/dist/main.js
@@ -1,25 +1,18 @@
 'use strict';
 
-var txt_body = document.getElementsByClassName('div-maintext')[0];
-var button = document.getElementById('btn-next');
+var txt_body = document.getElementsByClassName('div_text')[0];
+var next = document.getElementsByClassName('next')[0];
 
-button.addEventListener('click', function () {
-
+next.addEventListener('click', function () {
 	txt_body.style.left = '-565px';
 
 	setTimeout(function () {
-
 		if (txt_body.style.left === '-565px') {
-
 			txt_body.style.display = 'none';
-
 			setTimeout(function () {
-
 				txt_body.style.left = '565px';
 				txt_body.style.display = 'block';
-
 				setTimeout(function () {
-
 					txt_body.style.left = '0px';
 				}, 500);
 			}, 100);
