dc1e4592a002749a5efbb956c626069fb66c337e
b3n10 <francisben.lleve@gmail.com>
2018-06-13 21:35:57 +0800

    Add getProtocol method, redirect to error page with session error

--- resources/classes/Redirect.php
+++ resources/classes/Redirect.php
@@ -6,12 +6,18 @@ class Redirect {
 
 		switch ($loc) {
 		case 404:
-			header('Location: ../resources/errors/404.php');
+			$_SESSION['error'] = '404 Not Found';
+			// change to only /error/ on production
+			header('Location: ' . self::getProtocol() . $_SERVER['SERVER_NAME'] . '/randomQuotes/public_html/error/');
 			break;
 		default:
 			echo 'none';
 			break;
 		}
 
+	}
+
+	private static function getProtocol() {
+		return empty($_SERVER['HTTPS']) ? 'http://' : 'https://';
 	}
 }
