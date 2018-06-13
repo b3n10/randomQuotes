3963faad3c6d12d73e885678ef2126c3edbf3364
b3n10 <francisben.lleve@gmail.com>
2018-06-13 21:49:31 +0800

    Update link to css

--- public_html/error/index.php
+++ public_html/error/index.php
@@ -5,7 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<title><?php echo $_SESSION['error'] ?></title>
-	<link rel="stylesheet" href="../../public_html/src/css/style.css">
+	<link rel="stylesheet" href="../dist/css/style.css">
 </head>
 <body>
 	<div class="error">
