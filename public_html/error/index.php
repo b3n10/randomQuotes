e66f4f4be5e791cb345f9125f8e949c858355fa1
b3n10 <francisben.lleve@gmail.com>
2018-06-13 21:35:05 +0800

    Add main error page

---
+++ public_html/error/index.php
@@ -0,0 +1,16 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<title><?php echo $_SESSION['error'] ?></title>
+	<link rel="stylesheet" href="../../public_html/src/css/style.css">
+</head>
+<body>
+	<div class="error">
+		<h1><?php echo $_SESSION['error'] ?></h1>
+		<a href="/">..back to Home</a>
+	</div>
+</body>
+</html>
