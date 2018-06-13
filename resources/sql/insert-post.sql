6751a5c08f9d9ac96eefc2a71a03948fec62f23c
b3n10 <francisben.lleve@gmail.com>
2018-06-13 19:04:20 +0800

    Add sample posts

--- resources/sql/insert-post.sql
+++ resources/sql/insert-post.sql
@@ -7,4 +7,13 @@ INSERT INTO posts (
 ) VALUES (
 	"J.R.R. Tolkien",
 	"For myself, I find I become less cynical rather than more--remembering my own sins and follies; and realize that men's hearts are not often as bad as their acts, and very seldom as bad as their words."
+), (
+	"Galatians 2:20",
+	"I have been crucified with Christ and I no longer live, but Christ lives in me. The life I now live in the body, I live by faith in the Son of God, who loved me and gave himself for me."
+), (
+	"C. S. Lewis",
+	"Friendship is born at that moment when one person says to another: What! You too? I thought I was the only one."
+), (
+	"1 Timothy 1:15",
+	"The saying is trustworthy and deserving of full acceptance, that Christ Jesus came into the world to save sinners, of whom I am the foremost."
 );
