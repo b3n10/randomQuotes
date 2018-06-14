5e94d429b88120386a4906550dfa65a5c88cbb17
b3n10 <francisben.lleve@gmail.com>
2018-06-14 23:55:32 +0800

    Add submit.php

---
+++ public_html/submit.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+		<title>Project 1</title>
+		<link rel="stylesheet" href="./dist/css/style.css">
+	</head>
+	<body>
+	<div class="nav">
+		<div class="logo">
+			<a href="#">randomQuotes</a>
+		</div>
+	</div>
+	<div class="container">
+		<div class="div_mainbody">
+			<form action="" method="POST">
+				<div class="div_submit">
+					<input id="author" type="text" name="author" placeholder="Author of quote" autocomplete="off">
+				</div>
+				<div class="div_submit">
+					<textarea id="bodyText" name="bodyText" placeholder="Body of text"></textarea>
+				</div>
+				<div class="div_submit">
+					<button type="submit">Submit</button>
+					<button type="button">Clear</button>
+				</div>
+			</form>
+		</div>
+	</div>
+		<script src="">
+		</script>
+	</body>
+</html>
