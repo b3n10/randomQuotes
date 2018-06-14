b9f605ad1b569737f2e27cdb07e9d694470820de
b3n10 <francisben.lleve@gmail.com>
2018-06-15 00:02:46 +0800

    Change author limit to 30

--- resources/sql/create_db.sql
+++ resources/sql/create_db.sql
@@ -6,7 +6,7 @@ USE quotes;
 
 CREATE TABLE posts (
 	id					int(5)				not null AUTO_INCREMENT PRIMARY KEY,
-	author				VARCHAR(20),
+	author				VARCHAR(30),
 	text				VARCHAR(200),
 	approved 		tinyint(1)		default 0
 );
