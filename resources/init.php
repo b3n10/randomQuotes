729558b2c787b97f77e223932c9621a28ecd9133
b3n10 <francisben.lleve@gmail.com>
2018-06-25 20:39:41 +0800

    Delete source folder (only for development), add Postgres db credentials

--- resources/init.php
+++ resources/init.php
@@ -3,11 +3,11 @@
 session_start();
 
 $_SESSION['config'] = [
-	'host'			=>	'127.0.0.1',
-	'dbname'		=>	'quotes',
+	'host'			=>	'ec2-54-235-70-253.compute-1.amazonaws.com',
+	'dbname'		=>	'dq2ug9i8fp2h0',
 	'charset'		=>	'charset=utf8',
-	'user'			=>	'root',
-	'password'	=>	'jairah'
+	'user'			=>	'nblrtactkhqfmo',
+	'password'	=>	'7c3b903cfff2476f836ec606714d06fbc1aa6dbd664d612e876ee49ff586ca79'
 ];
 
 spl_autoload_register(function($class) {
