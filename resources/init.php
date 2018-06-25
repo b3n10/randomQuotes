22f5f41e3a51edf785f8407c149a7bf87507a695
b3n10 <francisben.lleve@gmail.com>
2018-06-25 22:09:50 +0800

    Test db 13

--- resources/init.php
+++ resources/init.php
@@ -2,25 +2,25 @@
 
 session_start();
 
-$db = parse_url(getenv("DATABASE_URL"));
-
-// $pdo = new PDO("pgsql:" . sprintf(
-//     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-//     $db["host"],
-//     $db["port"],
-//     $db["user"],
-//     $db["pass"],
-//     ltrim($db["path"], "/")
-// ));
-
-$_SESSION['config'] = [
-	'host'			=>	$db['host'],
-	'port'			=>	$db['port'],
-	'dbname'		=>	ltrim($db["path"], "/"),
-	'charset'		=>	'charset=utf8',
-	'user'			=>	$db['user'],
-	'password'	=>	$db['pass']
-];
+// $db = parse_url(getenv("DATABASE_URL"));
+//
+// // $pdo = new PDO("pgsql:" . sprintf(
+// //     "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+// //     $db["host"],
+// //     $db["port"],
+// //     $db["user"],
+// //     $db["pass"],
+// //     ltrim($db["path"], "/")
+// // ));
+//
+// $_SESSION['config'] = [
+// 	'host'			=>	$db['host'],
+// 	'port'			=>	$db['port'],
+// 	'dbname'		=>	ltrim($db["path"], "/"),
+// 	'charset'		=>	'charset=utf8',
+// 	'user'			=>	$db['user'],
+// 	'password'	=>	$db['pass']
+// ];
 
 spl_autoload_register(function($class) {
 	require_once "classes/" . $class . ".php";
