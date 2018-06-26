c38484762b3f0bd0a9009a28c94fe73cdfb16f4d
b3n10 <francisben.lleve@gmail.com>
2018-06-26 08:25:57 +0800

    TODO: Fix trim before submit

--- public/submit.php
+++ public/submit.php
@@ -6,9 +6,6 @@ if ($_POST) {
 	$author	= trim($_POST['author'], '-“\'"” ');
 	$text		= trim($_POST['bodyText'], '-“\'"” ');
 
-	$author	= trim($author, '-“\'"” ');
-	$text		= trim($text, '-“\'"” ');
-
 	var_dump($author, $text);
 	die();
 
